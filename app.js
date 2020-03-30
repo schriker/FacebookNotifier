c38f079616883d41cb1416da7c3bccbe8bec712e
schriker <janekmachine@gmail.com>
2020-03-30 16:30:21 +0200

    Restream to youtube test

--- app.js
+++ app.js
@@ -1,98 +1,105 @@
-const fs = require('fs')
 const axios = require('axios')
-const youtubeUpload = require('./youtube')
-const { exec, spawn } = require('child_process')
+const EventEmitter = require('events')
+const { spawn } = require('child_process')
+const config = require('./config.json')
 
-class FacebookNotifier {
+class Facebook extends EventEmitter {
   constructor(facebookId) {
+    super()
     this.facebookId = facebookId
     this.isOnline = null
+    this.error = false
     this.listener()
   }
 
   listener() {
     const interval = setInterval(async () => {
-      const facebookResponse = await axios.get(`https://www.facebook.com/pages/videos/search/?page_id=${this.facebookId}&__a`)
-      const facebookVideos = JSON.parse(facebookResponse.data.split('for (;;);').pop())
+      const facebookResponse = await axios.get(
+        `https://www.facebook.com/pages/videos/search/?page_id=${this.facebookId}&__a`
+      )
+      const facebookVideos = JSON.parse(
+        facebookResponse.data.split('for (;;);').pop()
+      )
       const lastVideo = facebookVideos.payload.page.video_data[0]
       const { viewCount, videoID } = lastVideo
       const isOnline = viewCount === '0'
+
       if (this.isOnline !== isOnline) {
         this.isOnline = isOnline
         if (this.isOnline) {
-          console.log(`[Online] - ${videoID} - ${new Date()}`)
-          this.isOnline = true
-          this.startDownload(videoID)
-          clearInterval(interval)
+          console.log(`Facebook: [Online] - ${videoID} - ${new Date()}`)
+          if (!this.error) {
+            this.emit('online')
+            this.startDownload(videoID)
+            clearInterval(interval)
+          }
         } else if (!this.isOnline) {
-          this.isOnline = false
-          console.log(`[Offline] - ${videoID} - ${new Date()}`)
+          console.log(`Facebook: [Offline] - ${videoID} - ${new Date()}`)
+          if (this.error) {
+            this.emit('uploaded', {
+              data: {
+                id: '',
+                snippet: {
+                  thumbnails: {
+                    medium: {
+                      url: ''
+                    }
+                  }
+                }
+              }
+            })
+          }
+          this.error = false
+          this.emit('offline')
         }
       }
     }, 1500)
   }
 
   async startDownload(videoID) {
-    const streamlink = spawn('streamlink', [`https://www.facebook.com/${this.facebookId}/videos/${videoID}`, 'best', '-O'])
-    const ffmpeg = spawn('ffmpeg', ['-i', 'pipe:0', '-c', 'copy', '-f', 'flv', 'pipe:1'])
-  
-    streamlink.stdout.on('data', (data) => {
-      ffmpeg.stdin.write(data);
-    });
-    
-    streamlink.stderr.on('data', (data) => {
-      console.error(`streamlink stderr: ${data}`);
-    });
-    
-    streamlink.on('close', (code) => {
-      if (code !== 0) {
-        console.log(`streamlink process exited with code ${code}`);
-      }
-      ffmpeg.stdin.end();
-    });
+    const streamlink = spawn('streamlink', [
+      `https://www.facebook.com/${this.facebookId}/videos/${videoID}`,
+      'best',
+      '-O'
+    ])
+    const ffmpeg = spawn('ffmpeg', [
+      '-re',
+      '-i',
+      'pipe:0',
+      '-c:v',
+      'copy',
+      '-f',
+      'flv',
+      `rtmp://a.rtmp.youtube.com/live2/${config.YT_API.STREAM_KEY}`
+    ])
 
-    // ffmpeg.stdout.on('data', (data) => {
-    //   console.log(data.toString());
-    // });
-    
-    ffmpeg.stderr.on('data', (data) => {
-      console.error(`ffmpeg stderr: ${data}`);
-    });
-    
-    ffmpeg.on('close', (code) => {
-      if (code !== 0) {
-        console.log(`ffmpeg process exited with code ${code}`);
-      }
-    });
-
-    // try {
-    //   await youtubeUpload(ffmpeg.stdout, { facebookId: videoID })
-    //   fs.unlinkSync(`${videoID}.ts`)
-    //   console.log(`Video - ${videoID} - uploaded - ${new Date()}`)
-    // } catch (err) {
-    //   console.log(err)
-    // }
-  }
-
-  async stopDownload(videoID) {
-    const comand = spawn('ffmpeg', ['-i', `${videoID}.ts`, '-c', 'copy', '-f', 'flv', 'pipe:1'])
-
-    comand.stderr.on('data', (data) => {
-      console.log(`stderr: ${data.toString()}`)
+    streamlink.stdout.on('data', data => {
+      ffmpeg.stdin.write(data)
     })
 
-    comand.on('close', (code) => {
-        console.log(`child process exited with code ${code}`)
+    streamlink.stderr.on('data', data => {
+      console.log(`streamlink: ${data}`)
     })
 
-    try {
-      await youtubeUpload(comand.stdout, { facebookId: videoID })
-      fs.unlinkSync(`${videoID}.ts`)
-      console.log(`Video - ${videoID} - uploaded - ${new Date()}`)
-    } catch (err) {
-      console.log(err)
-    }
+    streamlink.on('close', code => {
+      if (code !== 0) {
+        console.log(`streamlink process exited with code ${code}`)
+      }
+      ffmpeg.stdin.end()
+    })
+
+    ffmpeg.stderr.on('data', data => {
+      console.log(`ffmpeg: ${data}`)
+    })
+
+    ffmpeg.on('close', code => {
+      if (code !== 0) {
+        console.log(`ffmpeg process exited with code ${code}`)
+      }
+    })
   }
 }
 
-new FacebookNotifier('369632869905557')
+new Facebook('95475020353')
+
+module.exports = Facebook
